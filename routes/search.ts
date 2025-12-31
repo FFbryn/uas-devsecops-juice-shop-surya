1362e6ea4f3c99aefebc97a867e9fb36402914af
Surya Febriyan <febriyansurya41@gmail.com>
2025-12-31 12:28:27 +0000

    perbaikan xss

--- routes/search.ts
+++ routes/search.ts
@@ -4,7 +4,6 @@
  */
 
 import { type Request, type Response, type NextFunction } from 'express'
-
 import * as utils from '../lib/utils'
 import * as models from '../models/index'
 import { UserModel } from '../models/user'
@@ -20,19 +19,30 @@ export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
+
+    models.sequelize.query(
+      `SELECT * FROM Products
+       WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%')
+       AND deletedAt IS NULL)
+       ORDER BY name`
+    ) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
         const dataString = JSON.stringify(products)
+
+        /* =============================
+           LOGIKA CHALLENGE (JANGAN DIUBAH)
+        ============================== */
         if (challengeUtils.notSolved(challenges.unionSqlInjectionChallenge)) { // vuln-code-snippet hide-start
           let solved = true
           UserModel.findAll().then(data => {
             const users = utils.queryResultToJson(data)
             if (users.data?.length) {
               for (let i = 0; i < users.data.length; i++) {
-                solved = solved && utils.containsOrEscaped(dataString, users.data[i].email) && utils.contains(dataString, users.data[i].password)
-                if (!solved) {
-                  break
-                }
+                solved =
+                  solved &&
+                  utils.containsOrEscaped(dataString, users.data[i].email) &&
+                  utils.contains(dataString, users.data[i].password)
+                if (!solved) break
               }
               if (solved) {
                 challengeUtils.solve(challenges.unionSqlInjectionChallenge)
@@ -42,33 +52,45 @@ export function searchProducts () {
             next(error)
           })
         }
+
         if (challengeUtils.notSolved(challenges.dbSchemaChallenge)) {
           let solved = true
-          void models.sequelize.query('SELECT sql FROM sqlite_master').then(([data]: any) => {
-            const tableDefinitions = utils.queryResultToJson(data)
-            if (tableDefinitions.data?.length) {
-              for (let i = 0; i < tableDefinitions.data.length; i++) {
-                if (tableDefinitions.data[i].sql) {
-                  solved = solved && utils.containsOrEscaped(dataString, tableDefinitions.data[i].sql)
-                  if (!solved) {
-                    break
+          void models.sequelize.query('SELECT sql FROM sqlite_master')
+            .then(([data]: any) => {
+              const tableDefinitions = utils.queryResultToJson(data)
+              if (tableDefinitions.data?.length) {
+                for (let i = 0; i < tableDefinitions.data.length; i++) {
+                  if (tableDefinitions.data[i].sql) {
+                    solved =
+                      solved &&
+                      utils.containsOrEscaped(
+                        dataString,
+                        tableDefinitions.data[i].sql
+                      )
+                    if (!solved) break
                   }
                 }
+                if (solved) {
+                  challengeUtils.solve(challenges.dbSchemaChallenge)
+                }
               }
-              if (solved) {
-                challengeUtils.solve(challenges.dbSchemaChallenge)
-              }
-            }
-          })
+            })
         } // vuln-code-snippet hide-end
+
+        /* =============================
+           PERBAIKAN XSS (AMAN)
+        ============================== */
         for (let i = 0; i < products.length; i++) {
-          products[i].name = req.__(products[i].name)
-          products[i].description = req.__(products[i].description)
+          products[i].name = utils.escapeHtml(products[i].name)
+          products[i].description = utils.escapeHtml(products[i].description)
         }
+
         res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
+      })
+      .catch((error: ErrorWithParent) => {
         next(error.parent)
       })
   }
 }
 // vuln-code-snippet end unionSqlInjectionChallenge dbSchemaChallenge
+
